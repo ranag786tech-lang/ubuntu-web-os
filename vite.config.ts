7d63a4ceee307c73a753136d290786ca1ee29fc5
RANA Hamza <253780593+ranag786tech-lang@users.noreply.github.com>
2026-06-02 23:57:17 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,18 @@
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+import { inspectAttr } from 'plugin-inspect-react-code'
+
+// https://vite.dev/config/
 export default defineConfig({
-  base: '/ubuntu-web-os/',   // 👈 correct for GitHub Pages
+  base: './',
   plugins: [inspectAttr(), react()],
   server: {
     port: 3000,
   },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(dirname, "./src"),
     },
   },
 });
